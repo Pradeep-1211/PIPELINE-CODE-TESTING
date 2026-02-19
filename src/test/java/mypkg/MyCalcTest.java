d5544c2a7db96c284e639f35728e31c6b01835fe
Pradeep-1211 <pradeepsingh907157@gmail.com>
2026-02-19 18:45:50 +0530

    Update MyCalcTest.java

--- src/test/java/mypkg/MyCalcTest.java
+++ src/test/java/mypkg/MyCalcTest.java
@@ -11,8 +11,10 @@ public class MyCalcTest {
 	@Test
 	public void test() {
 		
-		assertEquals(70, calc.sum(40, 30));
-		assertEquals(10, calc.diff(40, 30));		
+		assertEquals(30, calc.sum(20, 10));
+		assertEquals(10, calc.diff(20, 10));
+		assertEquals(200, calc.mul(20, 10));
+		assertEquals(2, calc.div(20, 10));
 	}
 
 }
